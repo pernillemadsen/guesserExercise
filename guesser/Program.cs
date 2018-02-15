7c5fca996286e6f910bd07c8dd7c01dd3b66497e
Unknown <marco.prolog@gmail.com>
2018-02-15 13:14:27 +0100

    Added project and file to modify for the exercise

---
+++ guesser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guesser
+{
+    class Guesser
+    {
+        // the "const" keyword makes the value of the variable final, meaning it can never be modified
+        // a variable with "const" acts also as if it had the "static" modifier, meaning it can be accessed without needing to instantiate an object
+        const int MaxTrials = 10;
+
+        static void Main(string[] args)
+        {
+            /* TO DO: here is where you add your code! */
+        }
+    }
+}
