e021a4204ebc0e8fdda68b86c6668b292a2b81a5
Pernille Madsen <iris.crossblade@gmail.com>
2018-02-20 11:59:02 +0100

    Solution

--- guesser/Program.cs
+++ guesser/Program.cs
@@ -10,11 +10,40 @@ namespace guesser
     {
         // the "const" keyword makes the value of the variable final, meaning it can never be modified
         // a variable with "const" acts also as if it had the "static" modifier, meaning it can be accessed without needing to instantiate an object
-        const int MaxTrials = 10;
+
 
         static void Main(string[] args)
         {
-            /* TO DO: here is where you add your code! */
+            int maxTrials = 10;
+            int trials = 0;
+
+            Console.WriteLine("Try guessing the right name! You have 10 tries!");
+
+            while (true && trials <= maxTrials)
+            {
+                
+                string name = Console.ReadLine();
+                switch (name)
+                {
+                    case "Frida":
+                        Console.WriteLine("Congratulations!, the application will close in 3 seconds!");
+                        System.Threading.Thread.Sleep(3000);
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry, please try again");
+                        trials++;
+                        break;
+
+                }
+                if (trials == maxTrials)
+                {
+                    Console.WriteLine("You're out of guesses!, the application will close in 3 seconds!");
+                    System.Threading.Thread.Sleep(3000);
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
